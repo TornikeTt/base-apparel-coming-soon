384aa26d7d3475e1b208f30e0bc24134a43f1c27
Tornike <chargeishvilitornike@gmail.com>
2025-01-30 21:59:13 +0400

    add validation for input

---
+++ script.js
@@ -0,0 +1,29 @@
+const form = document.querySelector(".coming-soon form");
+const input = document.querySelector(".email");
+const errorMessage = document.querySelector(".error-message");
+const errorIcon = document.querySelector(".error-icon");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (input.value == "") {
+        errorMessage.innerText = "Email cannot be empty";
+        errorMessage.style.display = "block";
+        errorIcon.style.display = "block";
+        input.style.borderWidth = "2px";
+        input.style.borderColor = "red";
+    } else {
+        const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const isValid = regex.test(input.value);
+
+        if (isValid) {
+            errorMessage.innerText = "";
+            errorMessage.style.display = "none";
+            errorIcon.style.display = "none";
+        } else {
+            errorMessage.innerText = "Please provide a valid email";
+            errorMessage.style.display = "block";
+            errorIcon.style.display = "block";
+        }
+    }
+});
